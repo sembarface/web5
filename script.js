a53034563b16fca60cfe289e6f9279936d056468
sembarface <114759842+sembarface@users.noreply.github.com>
2024-10-10 18:45:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,13 @@ function calculateTotal() {
     const price = parseFloat(productSelect.value); // Получаем цену выбранного товара
     const quantity = parseInt(quantityInput.value); // Получаем количество товара
     const total = price * quantity; // Рассчитываем общую стоимость
-
-    result.textContent = `Стоимость: ${total} руб.`; // Выводим результат
+    if(total<0){
+        result.textContent = `ВЫ НЕ МОЖЕТЕ КУПИТЬ ОТРИЦАТЕЛЬНОЕ ЧИСЛО ЧЕГО ЛИБО`;
+    }
+    else{
+        result.textContent = `Стоимость: ${total} руб.`; // Выводим результат
+    }
+    
 }
 
 // Обработчик события нажатия на кнопку
